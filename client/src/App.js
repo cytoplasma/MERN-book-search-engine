15d2f53ede90048ff297381f316dcd9861175b78
cytoplasma <jpiromg@gmail.com>
2023-02-04 15:11:30 -0800

    updated package.json, export typedefs, rolvers && edited searchBook page

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
 
 const httpLink = createHttpLink({
-  uri: "/graphql"
+  uri: "/graphql",
 });
 
 const authLink = setContext((__, { headers }) => {
