616ddf2f38f8031d50e7e152827fbdacc7d84ecd
cytoplasma <jpiromg@gmail.com>
2023-02-04 17:55:23 -0800

    deployment fix attempt

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth')
 const path = require('path');
 const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./schemas');
 // const routes = require('./routes');
 
 const app = express();
